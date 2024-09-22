865ac4267487f62c8a2a0c726a9ae75a8f535cf3
bmitc <65685447+bmitc@users.noreply.github.com>
2024-09-22 12:15:59 -0400

    Implement `puresnmp`-based API

---
+++ cyberpower_pdu/scripts/get_all_outlet_states.py
@@ -0,0 +1,26 @@
+# Core dependencies
+import asyncio
+
+# Project dependencies
+from cyberpower_pdu import CyberPowerPDU
+
+IP_ADDRESS = "192.168.1.132"
+
+
+async def main() -> None:
+    pdu = CyberPowerPDU(ip_address=IP_ADDRESS, simulated=False)
+
+    try:
+        await pdu.initialize()
+
+        outlet_states = await pdu.get_all_outlet_states()
+
+        for index, outlet_state in enumerate(outlet_states):
+            print(f"Outlet {index + 1} state: {outlet_state}")
+
+    finally:
+        await pdu.close()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
